990baed1882bc6b214aa5529276f20464c80d347
BrunoPan08 <brunopan08@gmail.com>
2024-03-15 11:26:52 -0300

    fix layout of monitor

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,9 +5,9 @@ import { discount, robot, head02, start, head01 } from '../assets'
 
 const Hero = () => {
   return (
-    <section id="home" className={`flex md:flex-row flex-col sm:py-[71px] py-6 sm:px-10`}>
+    <section id="home" className={`flex md:flex-row flex-col xl:py-[20px] sm:py-[71px]  py-6 sm:px-10`}>
       <div className={`flex-1 ${styles.flexStart} flex-col xl:px-0 ml-[5rem]`}>
-        <h1 className='font-lato font-semibold ss:text-[34px] sm:pr-[13rem] text-[34px] mb-[20px] ss:leading-[54px] leading-[75px] w-full text-white'>Discover all possibilities of using this powerfull tool and create science figures, posters and infographics.</h1>
+        <h1 className='font-lato font-semibold ss:text-[34px] xl:pr-[10rem] sm:pr-[13rem] text-[34px] mb-[20px] ss:leading-[54px] leading-[75px] w-full text-white'>Discover all possibilities of using this powerfull tool and create science figures, posters and infographics.</h1>
         <img src={start} className='cursor-pointer'/>
       </div>
       <div className={`flex-1 flex ${styles.flexCenter} md:my-0 my-10 mr-[2rem] relative`}>
--- src/style.js
+++ src/style.js
@@ -12,7 +12,7 @@ const styles = {
     padding: "sm:px-16 px-6 sm:py-12 py-4",
 
     smallPaddingY: "sm:py-2 py-6",
-    smallPadding: "sm:px-[6rem] px-6 sm:py-12 py-4",
+    smallPadding: "xl:px-[5rem] sm:px-[6rem] px-6 sm:py-12 py-4",
   
     marginX: "sm:mx-16 mx-6",
     marginY: "sm:my-16 my-6",
